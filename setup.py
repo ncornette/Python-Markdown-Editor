438d2e422b3843857a8275f8a9d0c1a97132ceac
Nicolas Cornette <nicolas.cornette@gmail.com>
2016-05-25 01:55:31 +0200

    Rename package

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ for data_dir in data_dirs:
 
 datafiles.extend(r.sub('', f) for f in data_files)
 
-setup(name='Python-Markdown-Editor',
+setup(name='Markdown-Editor',
       version='0.9.1',
       description='Standalone editor for your markdown files',
       long_description=long_description,
