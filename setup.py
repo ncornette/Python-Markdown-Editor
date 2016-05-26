8ea29b1ee1dc4711c27f0587df5c616042747b1b
Nicolas Cornette <nicolas.cornette@gmail.com>
2016-05-27 00:57:13 +0200

    bump version 0.9.4

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ for data_dir in data_dirs:
 datafiles.extend(r.sub('', f) for f in data_files)
 
 setup(name='Markdown-Editor',
-      version='0.9.3',
+      version='0.9.4',
       description='Standalone editor for your markdown files',
       long_description=long_description,
       classifiers=[
